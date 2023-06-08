e553a411c0dd4972d7a56293689dfee4050df775
Lemur <piotr.libucha.off@gmail.com>
2023-06-08 23:47:05 +0200

    Add simple_rnn and change some preporcessing functions

--- preprocessing_functions.py
+++ preprocessing_functions.py
@@ -94,7 +94,7 @@ def get_transactions_out_with_adjacency_matrix(adjacency_matrix, root_transactio
 
 def get_adjacency_map(edges):
     adjacency_map = {}
-    for index, row in edges.iterrows():
+    for _, row in edges.iterrows():
         if not row['txId1'] in adjacency_map:
             adjacency_map[row['txId1']] = []
         if not row['txId2'] in adjacency_map:
@@ -104,20 +104,41 @@ def get_adjacency_map(edges):
     return adjacency_map
 
 
-def get_transactions_out_with_adjacency_map(adjacency_map, root_transaction, max_depth=None):
-    transactions_out = adjacency_map[root_transaction]
-    transaction_trees_out = []
-    if max_depth is None or max_depth > 0:
-        for i in transactions_out:
-            transaction_trees_out.append(get_transactions_out_with_adjacency_map(adjacency_map, i, max_depth - 1 if max_depth is not None else None))
-    else:
-        transaction_trees_out = 'max_depth reached'
-
-    transactions = {
+def get_transactions_out_with_adjacency_map(adjacency_map, root_transaction, max_depth=None, max_size=None):
+    transactions_main_tree = {
         'txId': root_transaction,
-        'transaction_trees_out': transaction_trees_out
+        'transaction_trees_out': []
     }
-    return transactions
+    search_queue = [root_transaction]
+    depth_queue = [0]
+    transaction_trees_queue = [transactions_main_tree]
+    size = 1
+
+    while True:
+        if len(search_queue) == 0:
+            break
+
+        transaction = search_queue.pop(0)
+        depth = depth_queue.pop(0) + 1
+        transaction_tree = transaction_trees_queue.pop(0)
+        transactions_out = adjacency_map[transaction]
+
+        if max_depth is not None and depth >= max_depth:
+            transaction_tree['transaction_trees_out'] = 'max_depth reached'
+        elif max_size is not None and size + len(transactions_out) >= max_size:
+            transaction_tree['transaction_trees_out'] = 'max_size reached'
+        else:
+            for i in transactions_out:
+                transaction_tree['transaction_trees_out'].append({
+                    'txId': i,
+                    'transaction_trees_out': []
+                })
+                search_queue.append(i)
+                depth_queue.append(depth)
+                transaction_trees_queue.append(transaction_tree['transaction_trees_out'][-1])
+                size += 1
+    
+    return transactions_main_tree
 
 
 def get_inversed_adjacency_map(edges):
@@ -132,17 +153,38 @@ def get_inversed_adjacency_map(edges):
     return inversed_adjacency_map
 
 
-def get_transactions_in_with_inversed_adjacency_map(inversed_adjacency_map, root_transaction, max_depth=None):
-    transactions_in = inversed_adjacency_map[root_transaction]
-    transaction_trees_in = []
-    if max_depth is None or max_depth > 0:
-        for i in transactions_in:
-            transaction_trees_in.append(get_transactions_in_with_inversed_adjacency_map(inversed_adjacency_map, i, max_depth - 1 if max_depth is not None else None))
-    else:
-        transaction_trees_in = 'max_depth reached'
-
-    transactions = {
+def get_transactions_in_with_inversed_adjacency_map(inversed_adjacency_map, root_transaction, max_depth=None, max_size=None):
+    transactions_main_tree = {
         'txId': root_transaction,
-        'transaction_trees_in': transaction_trees_in
+        'transaction_trees_in': []
     }
-    return transactions
+    search_queue = [root_transaction]
+    depth_queue = [0]
+    transaction_trees_queue = [transactions_main_tree]
+    size = 1
+
+    while True:
+        if len(search_queue) == 0:
+            break
+
+        transaction = search_queue.pop(0)
+        depth = depth_queue.pop(0) + 1
+        transaction_tree = transaction_trees_queue.pop(0)
+        transactions_in = inversed_adjacency_map[transaction]
+
+        if max_depth is not None and depth >= max_depth:
+            transaction_tree['transaction_trees_in'] = 'max_depth reached'
+        elif max_size is not None and size + len(transactions_in) >= max_size:
+            transaction_tree['transaction_trees_in'] = 'max_size reached'
+        else:
+            for i in transactions_in:
+                transaction_tree['transaction_trees_in'].append({
+                    'txId': i,
+                    'transaction_trees_in': []
+                })
+                search_queue.append(i)
+                depth_queue.append(depth)
+                transaction_trees_queue.append(transaction_tree['transaction_trees_in'][-1])
+                size += 1
+    
+    return transactions_main_tree
